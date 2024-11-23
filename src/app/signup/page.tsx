4cfc7ae1bef3e5690122e3ce800acea5492bb8db
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-11-23 17:56:06 +0530

    signupComp setup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,11 @@
 
+import SignUpComp from '@/components/signup/SignUpComp'
 import React from 'react'
 
 const SignUpPage = () => {
   return (
    <>
-   SignUpPage
+   <SignUpComp />
    </>
   )
 }
